2530863d339086fd88df06669e2ae824d00bf126
Yunus <ynstrn811@gmail.com>
2024-05-08 20:34:38 +0300

    Cloudinary integrated

--- 13-imaginify/app/(root)/layout.tsx
+++ 13-imaginify/app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Toaster />
       {/* Sidebar */}
       <Sidebar />
       {/* MobileNav*/}
